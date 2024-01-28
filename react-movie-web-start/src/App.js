6139d80ec3fe7e29080d0a6cdefcf4ef5339a6e3
hamilkarr <12uclock@gmail.com>
2024-01-29 00:27:59 +0900

    a little

--- react-movie-web-start/src/App.js
+++ react-movie-web-start/src/App.js
@@ -1,38 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-    const [toDo, setToDo] = useState('');
-    const [toDos, setToDos] = useState([]); // ['a', 'b', 'c']
-    const onChange = (event) => {
-        setToDo(event.target.value);
-    };
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (toDo === '') {
-            return;
-        }
-        setToDos((prevArray) => [...prevArray, toDo]);
-        setToDo('');
-    };
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch('https://api.coinpaprika.com/v1/tickers')
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+            });
+    }, []);
 
     return (
         <div>
-            <h1>To Do List ({toDos.length})</h1>
-            <form onSubmit={onSubmit} action="">
-                <input
-                    onChange={onChange}
-                    value={toDo}
-                    type="text"
-                    placeholder="Write your to do..."
-                />
-                <button>Add To Do</button>
-            </form>
-            <hr />
-            <ul>
-                {toDos.map((todo, index) => (
-                    <li key={index}>{todo}</li>
-                ))}
-            </ul>
+            <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
+            {loading ? (
+                <strong>Loading...</strong>
+            ) : (
+                <select>
+                    {coins.map((coin) => (
+                        <option key={coin.id}>
+                            {coin.name} ({coin.symbol}): ${' '}
+                            {coin.quotes.USD.price.toFixed(2)} USD
+                        </option>
+                    ))}
+                </select>
+            )}
         </div>
     );
 }

02292ca600f2472355e0c0e7b997d9eaf12fc759
hamilkarr <12uclock@gmail.com>
2024-01-28 23:02:47 +0900

    투두리스트 실습 완료

--- react-movie-web-start/src/App.js
+++ react-movie-web-start/src/App.js
@@ -1,27 +1,38 @@
-import { useState, useEffect } from 'react';
-
-function Hello() {
-    function byFn() {
-        console.log('destroy : (');
-    }
-    function hiFn() {
-        console.log('create : )');
-        return byFn;
-    }
-    useEffect(hiFn, []);
-
-    return <div>Hello</div>;
-}
+import { useState } from 'react';
 
 function App() {
-    const [showing, setShowing] = useState(false);
+    const [toDo, setToDo] = useState('');
+    const [toDos, setToDos] = useState([]); // ['a', 'b', 'c']
+    const onChange = (event) => {
+        setToDo(event.target.value);
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === '') {
+            return;
+        }
+        setToDos((prevArray) => [...prevArray, toDo]);
+        setToDo('');
+    };
 
     return (
         <div>
-            {showing ? <Hello /> : null}
-            <button onClick={() => setShowing((prev) => !prev)}>
-                {showing ? 'Hide' : 'Show'}
-            </button>
+            <h1>To Do List ({toDos.length})</h1>
+            <form onSubmit={onSubmit} action="">
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type="text"
+                    placeholder="Write your to do..."
+                />
+                <button>Add To Do</button>
+            </form>
+            <hr />
+            <ul>
+                {toDos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
         </div>
     );
 }

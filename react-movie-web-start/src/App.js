edf81bbaa3d36ebf82c1afadaff3b7140c28da71
hamilkarr <12uclock@gmail.com>
2024-01-28 22:31:51 +0900

    useEffect 수업 완료

--- react-movie-web-start/src/App.js
+++ react-movie-web-start/src/App.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react';
+
+function Hello() {
+    function byFn() {
+        console.log('destroy : (');
+    }
+    function hiFn() {
+        console.log('create : )');
+        return byFn;
+    }
+    useEffect(hiFn, []);
+
+    return <div>Hello</div>;
+}
+
 function App() {
+    const [showing, setShowing] = useState(false);
+
     return (
         <div>
-            <h1>Welcome back!!!</h1>
+            {showing ? <Hello /> : null}
+            <button onClick={() => setShowing((prev) => !prev)}>
+                {showing ? 'Hide' : 'Show'}
+            </button>
         </div>
     );
 }
